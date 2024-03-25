19878b62c2287fef8b4ecff0b4c836ae856a070b
Riday1 <rrriday100@gmail.com>
2024-03-25 14:16:36 +0600

    an amature header is created with logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Header.css';
-
+import logo from '../../images/logo.png'
 
 const Header = () => {
     return (
         <nav className='header'>
-            <a href="/home">Home</a>
-            <a href="/about">About</a>
-            <a href="/orders">Orders</a>
-            <a href="/contact">Contact</a>
+            <div>
+                <a href="/"><img src={logo} alt="" /></a>
+            </div>
+            <div>
+                <a href="/home">Home</a>
+                <a href="/about">About</a>
+                <a href="/orders">Orders</a>
+                <a href="/contact">Contact</a>
+            </div>
         </nav>
     );
 };
